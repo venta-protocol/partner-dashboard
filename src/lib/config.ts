640be87cd043a870ea540749c02b6cbbbf77e10e
energeticlee <justinleeyile@gmail.com>
2025-07-15 15:52:12 +0900

    remove resend

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -22,7 +22,6 @@ export const EMAIL_USER = process.env.EMAIL_SERVER_USER;
 export const EMAIL_PASSWORD = process.env.EMAIL_SERVER_PASSWORD;
 export const EMAIL_FROM = process.env.EMAIL_FROM;
 
-export const BASE_URI = process.env.BASE_URI;
 
 export const OP_PRIVATE_KEY = process.env.OP_PRIVATE_KEY;
 
--- src/lib/resend/resend.ts
+++
@@ -1,4 +0,0 @@
-import { Resend } from "resend";
-
-// Initialize the Resend object with the API key from environment variables
-export const resend = new Resend(process.env.RESEND_API_KEY as string);
